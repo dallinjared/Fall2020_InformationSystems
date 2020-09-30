995c86c448d6f1dbc14d50fec4e5a248b1fad51b
Dallin Jared <66185492+djared85@users.noreply.github.com>
2020-09-30 10:02:55 -0600

    Update OOP_Assignment.py

---
+++ OOP_Assignment.py
@@ -0,0 +1,12 @@
+class Customer:
+
+    def __init__(self, sCust_id, sFirst, sLast, sAddress1, sAddress2, sCity, sState, sZip, fBalance):
+        self.cust_id = sCust_id
+        self.first_name = sFirst
+        self.last_name = sLast
+        self.address1 = sAddress1
+        self.address2 = sAddress2
+        self.city = sCity
+        self.state = sState
+        self.zip = sZip
+        self.balance = fBalance
